37f63c14e7eeee772d36073d7a516fada98fc19c
Anshu Rai <raianshu8991@gmail.com>
2024-07-15 18:53:43 +0530

    Removed default values

--- index.mjs
+++ index.mjs
@@ -4,38 +4,22 @@ import fs from "fs";
 import { program } from "commander";
 import { discoverEndpoints, generatePostmanCollection } from "./lib/index.mjs";
 
-
 // Load configuration based on provided config file
 program
   .option("-c, --config <configPath>", "Specify config file path")
   .parse(process.argv);
 
 // Extract configPath from command-line arguments or fallback to default
-const configPath = program.config ;
+const configPath = program.config || "./config.json";
 
 // Read and parse the configuration file
 let config;
-
-if(configPath) {
-  try {
-    config = JSON.parse(fs.readFileSync(configPath, "utf8"));
-  } catch (err) {
-    console.error("Error reading or parsing configuration file:", err);
-    process.exit(1);
-  }
+try {
+  config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+} catch (err) {
+  console.error("Error reading or parsing configuration file:", err);
+  process.exit(1);
 }
-else {
-
-  // Set default value for config
-  config = {
-    directoryToScan: "./",
-    framework: "express",
-    objectInstance: "app",
-    baseUrl: "http::localhost:3000",
-    postmanCollectionFile: "./postman_collection.json",
-  };
-}
-
 
 // Function to run API discovery and collection generation
 async function runTool(writeCollection = false) {
